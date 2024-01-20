ad33c882174473f89b9f02c8b87c1d4c660a4d0f
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-01-20 23:32:47 +0100

    First Push to github

---
+++ nuxt.config.ts
@@ -0,0 +1,75 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: {
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in'
+    },
+
+    layoutTransition: {
+      name: 'layout',
+      mode: "out-in"
+    },
+    head: {
+      title: "Nepto Assets",
+      link: [
+        {
+          rel: 'preload',
+          href: 'https://cdn.shareaholic.net/assets/pub/shareaholic.js',
+          as: 'script'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://unpkg.com/aos@2.3.1/dist/aos.css',
+        },
+
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/img/logo.png'
+        }
+      ],
+
+      meta: [
+        {
+          name: 'shareaholic:site_id',
+          content: '833959d5dc1e891b01eaf8c799cc1602'
+        }
+      ],
+
+
+      script: [
+        { src: "https://s3.tradingview.com/tv.js", tagPosition: 'head' },
+        { src: 'https://unpkg.com/aos@2.3.1/dist/aos.js', tagPosition: 'bodyClose' },
+        {
+          src: "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit",
+          tagPosition: 'bodyClose',
+          async: 'true'
+        },
+        {
+          src: "/main.js",
+          tagPosition: 'bodyClose',
+          async: 'true'
+        },
+        {
+          hid: 'shareaholic-script',
+          src: 'https://cdn.shareaholic.net/assets/pub/shareaholic.js',
+          async: true,
+          defer: true // Optional: defer loading if necessary
+        }
+
+      ],
+    },
+
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/css/style.css', '~/assets/css/main.css', '@fortawesome/fontawesome-free/css/all.css',
+  '@splidejs/splide/css', 'animate.css/animate.css'],
+
+})
