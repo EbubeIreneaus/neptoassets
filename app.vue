99df5755bc7f1c57b324cf84f3649cdaa0fc8710
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-01-22 18:20:27 +0100

    Smartup Added

--- app.vue
+++ app.vue
@@ -8,6 +8,28 @@
 </template>
 
 <script setup>
+useHead({
+    script: [
+      {
+        type: 'text/javascript',
+        innerHTML: `
+          var _smartsupp = _smartsupp || {};
+          _smartsupp.key = 'cbe1609dff1a29261dbbe11ca5161cf9182da735';
+          window.smartsupp||(function(d) {
+            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+            c.type='text/javascript';c.charset='utf-8';c.async=true;
+            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+          })(document);
+        `,
+      },
+    ],
+    noscript: [
+      {
+        innerHTML: 'Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a>',
+      },
+    ],
+})
 const api = "https://nepto-api.vercel.app/"
 // const api = "http://127.0.0.1:8000/"
 onMounted(() => {
