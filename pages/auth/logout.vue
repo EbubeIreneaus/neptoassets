a14a7697d9cc4f73838778c3bd930ad33a99d253
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-01-23 09:31:22 +0100

    logout redirect changed

--- pages/auth/logout.vue
+++ pages/auth/logout.vue
@@ -1,9 +1,12 @@
 <script setup>
+definePageMeta({
+    layout:''
+})
 import Cookies from 'js-cookie'
 if (Cookies.get('profileId')) {
     Cookies.remove('profileId')
 }
-useRouter().push('/auth/login')
+useRouter().push('/')
 onBeforeMount(() => {
 
 })
