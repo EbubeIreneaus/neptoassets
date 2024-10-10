23bfb79def2c771550f171c1c0cbc7bd61f53f96
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2024-10-11 00:45:48 +0100

    h-provider removed

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,3 @@
 export default defineNuxtRouteMiddleware((to, from)=>{
-    if(to.path.startsWith('/user') || to.path.startsWith('/auth')){
-        return navigateTo('/provider/host-error')
-    }
+
 })
